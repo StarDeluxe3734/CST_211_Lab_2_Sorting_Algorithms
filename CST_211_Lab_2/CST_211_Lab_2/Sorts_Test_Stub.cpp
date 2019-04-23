c36bb644401f4e8dc440733820f1345286bc8ff9
David Abaroa <david.abaroa@oit.edu>
2019-04-22 20:06:06 -0700

    Initialized Project - Test Stub

---
+++ CST_211_Lab_2/CST_211_Lab_2/Sorts_Test_Stub.cpp
@@ -0,0 +1,23 @@
+/*************************************************************
+* Author: David Abaroa
+* Assignment: Lab 2 Sorting Algorithms
+* Filename: Sorts_Test_Stub.cpp
+* Date Created: 4/22/2019
+**************************************************************/
+/*************************************************************
+* Lab/Assignment: Lab 2
+*
+* Overview:
+* The purpose of this lab is to create and see how different sorting algorithms behave.
+*
+* Input:
+* argv is the number of items to generate and sort.
+*
+* Output:
+* The output of this program will be the outputs of all the tests.
+************************************************************/
+
+int main(int argc, const char* argv[])
+{
+	return 0;
+}
